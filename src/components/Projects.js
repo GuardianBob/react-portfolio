d1842fadbb169e8c75c1f8fef090b0c64f5c0796
GuardianBob <78508863+GuardianBob@users.noreply.github.com>
2022-03-25 14:31:44 -0400

    update projects bg color (incomplete)

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,35 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { projects } from "../data";
 
 export default function Projects() {
+    const [altBG, setAltBG] = useState(false);
+
+    const getBG = () => {
+        let newBG = ""
+        altBG === false ? newBG = "bg-black" : newBG = "bg-white";
+        setAltBG(!altBG);
+        console.log(newBG);
+        return newBG;
+    }
+
     return(
         <section id='projects'>
             <div className='row mx-auto'>
-                <div className='col-md-12'>
+                <div className='col-md-10'>
                     <h1>Projects</h1>
                     <p>
                         Summary of projects...
                     </p>
                 </div>
             </div>
-            <div className='row mx-auto'>
-                {projects.map((project) => (
+            <div className='row mx-auto justify-content-around'>
+                {projects.map((project) => (                    
                     <a
                         href={project.link}
                         key={project.image}
-                        className='col-md-4'>
+                        className={`col-md-6 ${getBG()}`}>
                         <div className=''>
                             <img 
                                 alt={project.title}
-                                className=''
+                                className='mx-auto d-block'
                                 src={require(`../images/${project.image}`)}
                                 width="100"
                             />
                             <div className="">
-                                <h2 className="">
+                                <h2 className="text-center">
                                     {project.subtitle}
                                 </h2>
-                                <h1 className="">
+                                <h1 className="text-center">
                                     {project.title}
                                 </h1>
                                 <p className="">{project.description}</p>
